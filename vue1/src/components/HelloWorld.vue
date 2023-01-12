c44d95a0aa5d1e20f6290dec7ab43de0c65c00d9
Максим Шульгин <shulgin3@gmail.com>
2023-01-12 13:05:32 +0700

    12/01/23Less.3

--- vue1/src/components/HelloWorld.vue
+++ vue1/src/components/HelloWorld.vue
@@ -3,10 +3,10 @@ export default {
   props: ['msg'],
   data() {
     return {
+      timerId: -1,
       currentTextIndex: 0,
       text: '',
       greetingTexts: [
-        'Заходи, слюшай!',
         'You’ve successfully created a project',
         'Vue’s official documentation provides you with all information you need to get started',
         'Got stuck? Ask your question on Vue Land, our official Discord server, or StackOverflow',
@@ -16,12 +16,18 @@ export default {
   },
   mounted() {
     this.text = this.greetingTexts[0];
+    this.timerId = setTimeout(this.changeText, 2000);
+  },
+  unmounted() {
+    clearTimeout(this.timerId);
   },
   methods: {
     changeText() {
+      clearTimeout(this.timerId);
       this.currentTextIndex++;
       if (this.currentTextIndex == this.greetingTexts.length) this.currentTextIndex = 0;
       this.text = this.greetingTexts[this.currentTextIndex];
+      this.timerId = setTimeout(this.changeText, 2000);
     }
   }
 }
@@ -31,6 +37,7 @@ export default {
   <div class="greetings">
     <h1 class="green" @click="changeText">{{ msg }}</h1>
     <h3>
+      {{ timerId }}
       {{ text }}
     </h3>
   </div>
