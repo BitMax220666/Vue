958abb914fba7641e6bb072a32c4176ad7317c6b
Максим Шульгин <shulgin3@gmail.com>
2023-01-17 13:28:54 +0700

    17/01/23-13:30

--- vue1/src/App.vue
+++ vue1/src/App.vue
@@ -4,49 +4,64 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <a-layout>
+  <a-layout style="min-height: 100vh">
     <a-layout-sider width="200">
       <a-menu id="dddddd" v-model:openKeys="openKeys" v-model:selectedKeys="selectedKeys"
         :style="{ height: '100%', borderRight: 0 }" mode="inline" @click="handleClick">
-        <a-sub-menu key="sub1" @titleClick="titleClick">
-          <template #icon></template>
-          <MailOutlined />
 
+        <a-sub-menu key="sub1" @titleClick="titleClick">
+          <template #icon>
+            <MailOutlined />
+          </template>
+          
           <template #title>Navigation One</template>
+
           <a-menu-item-group key="g1">
-            <template #icon></template>
+            <template #icon>
               <QqOutlined />
+            </template>
+              
             <template #title>Item 1</template>
             <a-menu-item key="1">Option 1</a-menu-item>
             <a-menu-item key="2">Option 2</a-menu-item>
           </a-menu-item-group>
+
           <a-menu-item-group key="g2" title="Item 2">
             <a-menu-item key="3">Option 3</a-menu-item>
             <a-menu-item key="4">Option 4</a-menu-item>
           </a-menu-item-group>
+
         </a-sub-menu>
+
         <a-sub-menu key="sub2" @titleClick="titleClick">
           <template #icon>
             <AppstoreOutlined />
           </template>
+
           <template #title>Navigation Two</template>
           <a-menu-item key="5">Option 5</a-menu-item>
           <a-menu-item key="6">Option 6</a-menu-item>
+
           <a-sub-menu key="sub3" title="Submenu">
             <a-menu-item key="7">Option 7</a-menu-item>
             <a-menu-item key="8">Option 8</a-menu-item>
           </a-sub-menu>
+
         </a-sub-menu>
+
         <a-sub-menu key="sub4">
           <template #icon>
             <SettingOutlined />
           </template>
+
           <template #title>Navigation Three</template>
           <a-menu-item key="9">Option 9</a-menu-item>
           <a-menu-item key="10">Option 10</a-menu-item>
           <a-menu-item key="11">Option 11</a-menu-item>
           <a-menu-item key="12">Option 12</a-menu-item>
+
         </a-sub-menu>
+
       </a-menu>
     </a-layout-sider>
     <a-layout>
@@ -124,4 +139,4 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
-</style> -->
+</style=> -->
